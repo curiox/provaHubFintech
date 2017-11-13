5347ea11ff94a97995dc00c2c66796e44913f18d
curiox <jun.ichizaki@gmail.com>
2017-11-12 23:24:34 -0200

    Inicio de implementação do back-end

--- WebContent/js/conta.js
+++ WebContent/js/conta.js
@@ -1,11 +1,26 @@
 $("#addConta").click(function() {
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+
+	if(dd<10) {
+	    dd = '0'+dd
+	} 
+
+	if(mm<10) {
+	    mm = '0'+mm
+	} 
+
+	today = dd + '/' + mm + '/' + yyyy;
 	
 	$.post("/database/conta",
 	{
 		nome: $("#Nome").val(),
 		cnpj: $("#CNPJ").val(),
 		cpf: $("#CPF").val(),
-		tipoConta: $("#tipoConta").val()
+		tipoConta: $("#tipoConta").val(),
+		data: today
 	},
 	function (data, status) {
 		
--- src/org/provaHubFintech/server/ContaServerResource.java
+++ src/org/provaHubFintech/server/ContaServerResource.java
@@ -3,11 +3,22 @@ package org.provaHubFintech.server;
 import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+
 import org.restlet.resource.Delete;
 import org.restlet.resource.Patch;
 import org.restlet.resource.ServerResource;
+import org.provaHubFintech.controller.ConnectionProvider;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.Status;
 
 public class ContaServerResource extends ServerResource {
+	
+	private static int contador = 1;
 
 	@Get
 	public void consulta() {
@@ -16,7 +27,32 @@ public class ContaServerResource extends ServerResource {
 	
 	@Post
 	public void adiciona() {
-		
+		Connection c = null;
+		Request req = getRequest();
+		String nome = (String) req.getAttributes().get("nome"),
+				cnpj = (String) req.getAttributes().get("cnpj"),
+				cpf = (String) req.getAttributes().get("cpf"),
+				tipoConta = (String) req.getAttributes().get("tipoconta");
+		Date date = (Date) req.getAttributes().get("data");
+		try {
+			c = ConnectionProvider.getConnection();
+			PreparedStatement ps = c.prepareStatement("INSERT INTO CONTA VALUES (?, ?, ?, ?, ?, ?);");
+			ps.setInt(1, contador++);
+			ps.setString(2, nome);
+			ps.setDate(3, date);
+			ps.setString(4, cnpj);
+			ps.setString(5, cpf);
+			ps.setString(6, tipoConta);
+			int rowsAffected = ps.executeUpdate();
+			Response resp = getResponse();
+			resp.getAttributes().putIfAbsent("rowsAffected", rowsAffected);
+			resp.setStatus(Status.SUCCESS_ACCEPTED);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			Response res = getResponse();
+			res.setStatus(Status.CLIENT_ERROR_EXPECTATION_FAILED);
+			res.abort();
+		}
 	}
 	
 	@Delete
