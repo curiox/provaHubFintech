689aa5ddc2d6e894908574c6e7f43753f5418dc2
curiox <jun.ichizaki@gmail.com>
2017-11-14 11:14:22 -0200

    Alteração no script do BD da tabela Conta
    Adição do campo "atividade"

--- sqlScripts/provaConta.sql
+++ sqlScripts/provaConta.sql
@@ -5,6 +5,7 @@ CREATE TABLE `conta` (
   `CNPJ` varchar(14) DEFAULT NULL,
   `CPF` varchar(11) DEFAULT NULL,
   `tipoConta` varchar(1) DEFAULT NULL,
+  `atividade` tinyint(4) NOT NULL,
   PRIMARY KEY (`idConta`),
   KEY `CPF_idx` (`CNPJ`),
   CONSTRAINT `CNPJ` FOREIGN KEY (`CNPJ`) REFERENCES `pessoa_juridica` (`CNPJ`) ON DELETE NO ACTION ON UPDATE NO ACTION,
